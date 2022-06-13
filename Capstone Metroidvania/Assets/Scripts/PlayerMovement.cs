138833199f4e21b80a96ed86a04acd2d26c012bf
plazman97 <panarom@wit.edu>
2022-06-13 19:10:51 -0400

    Implemented Idle and Run animations

--- Capstone Metroidvania/Assets/Scripts/PlayerMovement.cs
+++ Capstone Metroidvania/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -17,7 +18,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-    
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
