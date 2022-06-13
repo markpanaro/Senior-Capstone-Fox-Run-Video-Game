a7f8ee96b8ca69154b989f4201b28a102d086df6
plazman97 <panarom@wit.edu>
2022-06-12 23:44:25 -0400

    Implemented GameScene and initial PlayerController2D
    Set up GameScene, initial commit of player

---
+++ Capstone Metroidvania/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("GameHandler.Start");
+
+        //int i = 0;
+        //FunctionPeriodic.Create(() => {
+          //  CMDebug.TextPopupMouse("Ding "+i+"!");
+            //i++;
+        //}, .5f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
