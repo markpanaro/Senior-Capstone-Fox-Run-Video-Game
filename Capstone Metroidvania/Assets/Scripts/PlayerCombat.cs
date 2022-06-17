f7a47357833d1976ed032fc02be0667386ace95b
plazman97 <panarom@wit.edu>
2022-06-17 00:36:02 -0400

    Implemented Player Attack
    Player can now attack and register hits (visible in console) though there are no damage-able targets. The gizmo to display the range of the attack visually is not working, will look into later

--- Capstone Metroidvania/Assets/Scripts/PlayerCombat.cs
+++ Capstone Metroidvania/Assets/Scripts/PlayerCombat.cs
@@ -7,6 +7,10 @@ public class PlayerCombat : MonoBehaviour
 
     public Animator animator;
 
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
     // Update is called once per frame
     void Update()
     {
@@ -22,8 +26,20 @@ public class PlayerCombat : MonoBehaviour
         //Play an attack animation
         animator.SetTrigger("Attack");
         //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         //Damage in range enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
 
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 
     }
 }
