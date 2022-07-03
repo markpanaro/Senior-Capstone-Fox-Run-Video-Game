49f9fe9d9d52b858f8c9d10bc30d3c1a71ff4f03
plazman97 <panarom@wit.edu>
2022-07-03 12:58:19 -0400

    Implemented Significantly Improved Scene Changing System

---
+++ Capstone Metroidvania/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    public static CameraController instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
